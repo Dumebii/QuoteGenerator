636b7b5d7de818e8649afd6ee8e82362cdd18b75
Dumebi Okolo <okolodumebi@gmail.com>
2022-09-04 01:02:39 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <p>
+        {" "}
+        Get more quotes at{" "}
+        <a href="https://www.goodreads.com/quotes/tag/inspirational">
+          GoodReads
+        </a>
+        .{" "}
+      </p>
+      <p> Thank you for coming. Much love ❤❤❤❤ </p>
+    </div>
+  );
+}
+
+export default Footer;
