75e8033e4d7ed43325fe7e183233bec7a95d1db2
Dumebi Okolo <okolodumebi@gmail.com>
2022-09-04 01:03:34 +0100

    Create Heading.jsx

---
+++ src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div>
+      <h1> Have you been feeling low on motivatiion? </h1>
+      <h6> We know exactly what you need </h6>
+    </div>
+  );
+}
+
+export default Heading;
