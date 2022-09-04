e99cf12f084c50efcb8ca26ce5c40ab6237f1714
Dumebi Okolo <okolodumebi@gmail.com>
2022-09-04 01:00:33 +0100

    Update quotes.js

--- src/quotes.js
+++ src/quotes.js
@@ -11,6 +11,30 @@ const quotes = [
   {
     quote: "Be the change that you wish to see in the world.",
     author: "Mahatma Gandhi"
+  },
+  {
+    quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+    author: "Martin Luther King jr"
+  },
+  {
+    quote: "We accept the love we think we deserve.",
+    author:"Steven Chbosky"
+  },
+  {
+    quote: "Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring.",
+    author:"Marilyn Monroe"
+  },
+  {
+    quote: "Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.",
+    author:"Neil Gaiman"
+  },
+  {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    author:"Thomas Eddison"
   }
 ];
 
